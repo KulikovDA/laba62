2c39f35c26e38e48e0cb10fffb60efe3c66a7742
Arion <Arion@DESKTOP-9BU80RS>
2023-12-10 19:27:34 +0200

    Добавьте файлы проекта.

---
+++ laba62/Program.cs
@@ -0,0 +1,33 @@
+﻿try
+{
+    Console.Write("Введите количество строк: ");
+    int n = int.Parse(Console.ReadLine());
+
+    Console.Write("Введите количество столбцов: ");
+    int m = int.Parse(Console.ReadLine());
+
+    int[,] matrix = new int[n, m];
+    int[] columnSums = new int[m];
+
+    Console.WriteLine("Введите элементы матрицы:");
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i, j] = int.Parse(Console.ReadLine());
+            columnSums[j] += matrix[i, j];
+        }
+    }
+
+    Console.WriteLine("Суммы элементов столбцов:");
+
+    for (int j = 0; j < m; j++)
+    {
+        Console.WriteLine($"Сумма столбца {j + 1}: {columnSums[j]}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
